673ec5d670adabfe6778a99e965309942eb07702
Spencer Gerritsen <spencergerritsen@gmail.com>
2023-01-22 00:07:05 -0700

    refactoring GM.js

--- README.md
+++ README.md
@@ -1,37 +1,4 @@
-# team profile generator
-  [![MIT license](https://img.shields.io/badge/License-MIT-yellow.svg)](https://lbesson.mit-license.org/)
-  ## Table of Contents
-  * [Description](#description)
-  * [Installation](#installation)
-  * [Usage](#usage)
-  * [Licenses](#licenses)
-  * [Contributing](#contributing)
-  * [Tests](#tests)
-  * [Questions](#questions)
-  * [Credits](#credits)
 
-
-
-  ## Description <a name="description"></a>
-  a node.js application that builds a webpage based on employee information for the following roles, engineer, manager, and intern.
-  ## Product Example
-  This is the Read.me end result after one has been generated.
-  https://drive.google.com/file/d/1HTacAFl4KpTHHru6WDX5DDeaFb4kQKV6/view?usp=sharing
-## Installation <a name="installation"></a>
-clone the repo or download the file and run in visual studio code
-## Usage <a name="usage"></a>
-npm i && npm i inquirer - then run "node index.js"
-## Licenses <a name="licenses"></a>
-  ## Licenses
-    This project is covered under the MIT license. To learn more about what this means, click the license button at the top.
-## Contributing <a name="contributing"></a>
-Give feedback at my email
-## Tests <a name="tests"></a>
-run the tests using jest
-## Questions <a name="questions"></a>
-Any questions about the project?
-Github: https://github.com/sppencerr
-Email: spencergerritsen@gmail.com
-## Credits <a name="credits"></a>
-build-my-team
-
+make me a readme
+![GitHub license](https://img.shields.io/badge/license-Open Software License 3.0-blue.svg)
+...
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,48 +1,96 @@
-import fs from 'fs';
+const inquirer = require('inquirer');
+const generateMarkdown = require('./utils/generateMarkdown');
+const fs = require('fs');
 
-const renderLicenseBadge = (license) => {
-  if (!license) return '';
-  return `[![${license} license](https://img.shields.io/badge/License-${license}-yellow.svg)](https://opensource.org/licenses/${license})`;
-};
+const questions = [
+    {
+        type: 'input',
+        name: 'name',
+        message: 'What is your name?',
+        validate: nameInput => !!nameInput || 'Please enter your name.'
+    },
+    {
+        type: 'input',
+        name: 'github',
+        message: 'Enter your GitHub Username',
+        validate: githubInput => !!githubInput || 'Please enter your GitHub username.'
+    },
+    {
+        type: 'input',
+        name: 'title',
+        message: 'Enter your GitHub repository name (use dash instead of spaces)(Case Sensitive)',
+        validate: titleInput => !!titleInput || 'Please enter your GitHub repository.'
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'Enter your email',
+        validate: emailInput => !!emailInput || 'Please enter your E-mail.'
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Enter a project description',
+        validate: descriptionInput => !!descriptionInput || 'Please enter a project description.'
+    },
+    {
+        type: 'input',
+        name: 'link',
+        message: 'Enter a Live Link to the project',
+        validate: linkInput => !!linkInput || 'Please enter a Live Link to the project.'
+    },
+    {
+        type: 'input',
+        name: 'install',
+        message: 'Enter installation instructions for the project',
+        validate: installInput => !!installInput || 'Please enter installation instructions for the project.'
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Enter usage information for the project',
+        validate: usageInput => !!usageInput || 'Please enter usage information for the project.'
+    },
+    {
+        type: 'input',
+        name: 'contributing',
+        message: 'Enter contribution information for the project',
+        validate: contributingInput => !!contributingInput || 'Please enter contribution information for the project.'
+    },
+    {
+        type: 'input',
+        name: 'tests',
+        message: 'Enter test information for the project',
+        validate: testsInput => !!testsInput || 'Please enter test information for the project.'
+    },
+    {
+        type: 'confirm',
+        name: 'confirmLicense',
+        message: 'Would you like to enter some information about the License for a "License" section?',
+        default: true
+    },
+    {
+        type: 'list',
+        name: 'license',
+        when: answers => answers.confirmLicense,
+        message: 'Select a license for your project:',
+        choices: ['MIT', 'Apache 2.0', 'GPL 3.0', 'BSD 3', 'None']
+    },
+    {
+      type: 'input',
+      name: 'licenseName',
+      message: 'Enter the full name of the license you selected:',
+      when: answers => answers.confirmLicense && answers.license !== 'None'
+  },
+  {
+      type: 'input',
+      name: 'questions',
+      message: 'Enter your contact information for questions and support:',
+  }
+];
 
-const renderLicenseSection = (license) => {
-  if (!license) return '';
-  return `## Licenses
-  This project is covered under the ${license} license. To learn more about what this means, click the license badge above.`;
-};
-
-const generateMarkdown = (data) => {
-  return `# ${data.title}
-  ${renderLicenseBadge(data.license)}
-  ## Table of Contents
-  * [Description](#description)
-  * [Installation](#installation)
-  * [Usage](#usage)
-  * [Licenses](#licenses)
-  * [Contributing](#contributing)
-  * [Tests](#tests)
-  * [Questions](#questions)
-  * [Credits](#credits)
-
-  ## Description <a name="description"></a>
-  ${data.description}
-  ## Installation <a name="installation"></a>
-  ${data.installation}
-  ## Usage <a name="usage"></a>
-  ${data.usage}
-  ## Licenses <a name="licenses"></a>
-  ${renderLicenseSection(data.license)}
-  ## Contributing <a name="contributing"></a>
-  ${data.contributing}
-  ## Tests <a name="tests"></a>
-  ${data.tests}
-  ## Questions <a name="questions"></a>
-  Any questions about the project?
-  Github: https://github.com/${data.github}
-  Email: ${data.email}
-  ## Credits <a name="credits"></a>
-  ${data.name}
-`;
-};
-
-export default generateMarkdown;
+inquirer.prompt(questions).then(answers => {
+  const markdown = generateMarkdown(answers);
+  fs.writeFileSync('README.md', markdown);
+  console.log('README.md generated successfully!');
+});
