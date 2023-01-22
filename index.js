f51e6a7ed26c5c2fa0d410853c1bf2769017b52a
Spencer Gerritsen <spencergerritsen@gmail.com>
2023-01-21 23:32:20 -0700

    better functionality

--- index.js
+++ index.js
@@ -1,162 +1,76 @@
-// packages needed.
-const fs = require("fs");
-const inquirer = require("inquirer");
-const generateMarkdown = require("./utils/generateMarkdown");
-// array of questions for input
-const questions = [{
-    type: "input",
-    name: "name",
-    message: "WELCOME! This is the README generator! What is the name of your project?",
-    validate: nameInput => {
-        if (nameInput) {
-            return true;
-        } else {
-            console.log("Please enter your name to credit yourself for your work");
-            return false;
-        }
-    }
-},
-{
-    type: "input",
-    name: "github",
-    message: "Please enter your GitHub username:",
-    validate: githubInput => {
-        if (githubInput) {
-            return true;
-        } else {
-            console.log("Please provide your github so that others may find all your work if they wish to do so.");
-            return false;
-        }
-    }
-},
-{
-    type: "input",
-    name: "email",
-    message: "Please enter your email address:",
-    validate: emailInput => {
-        if (emailInput) {
-            return true;
-        } else {
-            console.log("Please provide a way for people to contact you if they have any questions.");
-            return false;
-        }
-    }
-},
-{
-    type: "input",
-    name: "title",
-    message: "What is the title of your project?",
-    validate: titleInput => {
-        if (titleInput) {
-            return true;
-        } else {
-            console.log("You must include a title...");
-            return false;
-        }
-    }
-},
-{
-    type: "input",
-    name: "description",
-    message: "Please enter your project description here:",
-    validate: descriptionInput => {
-        if (descriptionInput) {
-            return true;
-        } else {
-            console.log("Please provide a description for your project.");
-            return false;
-        }
-    }
-},
-{
-    type: "input",
-    name: "installation",
-    message: "What are the steps of installation?",
-    validate: installationInput => {
-        if (installationInput) {
-            return true;
-        } else {
-            console.log("Please provide instructions for installation this will allow users to have the correct programs needed.");
-            return false;
-        }
-    }
-},
-{
-    type: "input",
-    name: "usage",
-    message: "Instructions for usage:",
-    validate: usageInput => {
-        if (usageInput) {
-            return true;
-        } else {
-            console.log("Pleaes provide instructions for usage, this will allow users to navigate your program better.");
-            return false;
-        }
-    }
-},
-{
-    type: "input",
-    name: "contributing",
-    message: "How can other people contribute to this project?",
-    validate: contributionInput => {
-        if (contributionInput) {
-            return true;
-        } else {
-            console.log("Please provide steps on how others can contribute to your project.");
-            return false;
-        }
-    }
-},
-{
-    type: "input",
-    name: "tests",
-    message: "Describe the tests written for your application and how to use them:",
-    validate: testsInput => {
-        if (testsInput) {
-            return true;
-        } else {
-            console.log("Please provide tests used to contribute to your project.");
-            return false;
-        }
-    }
-},
-{
-    type: "confirm",
-    name: "confirmLicenses",
-    message: "Would you like to include a license?",
-    default: false
-},
-{
-    type: "list",
-    name: "licenses",
-    message: "What license would you like to include?",
-    choices: ["MIT", "GPL", "CC--0"],
-    when: ({ confirmLicenses }) => {
-        if (confirmLicenses) {
-            return true;
-        } else {
-            return false;
-        }
-    }
-},
-];
+import inquirer from 'inquirer';
+import fs from 'fs';
+import util from 'util';
 
-// function to write the readme file
-function writeToFile(fileName, data) {
-    fs.writeFile(fileName, data, function(err) {
-        console.log(data), 
-        err ? console.log(err) : console.log("README.md created!");
-    })
-}
+const writeFileAsync = util.promisify(fs.writeFile);
 
-// initialize app
-function init() {
-    inquirer.prompt(questions)
-    .then(function(data) {
-        writeToFile("README.md", generateMarkdown(data));
-        console.log(data.license);
-    })
-}
+const questions = async () => {
+  const answers = await inquirer.prompt([
+    {
+      type: 'input',
+      name: 'author',
+      message: 'What is the name of the author?'
+    },
+    {
+      type: 'input',
+      name: 'username',
+      message: 'What is your GitHub username?'
+    },
+    {
+      type: 'input',
+      name: 'email',
+      message: 'What is your email address?'
+    },
+    {
+      type: 'input',
+      name: 'title',
+      message: 'What is the name of your project?'
+    },
+    {
+      type: 'input',
+      name: 'description',
+      message: 'Please provide a brief description of your project'
+    },
+    {
+      type: 'list',
+      name: 'license',
+      message: 'What type of license will your project need (if any)?',
+      choices: [
+        'Apache 2.0',
+        'MIT',
+        'Open Software License 3.0',
+        'Educational Community License v2.0',
+        'None'
+      ]
+    },
+    {
+      type: 'input',
+      name: 'installation',
+      message: 'What are the instructions for installation?'
+    },
+    {
+      type: 'input',
+      name: 'contribution',
+      message: 'Users are free to contribute to this application.'
+    }
+  ]);
+  
+  return answers;
+};
 
-// call initialize app.
-init();
+const generateMD = (data) => {
+  return `
+${data.title}
+![GitHub license](https://img.shields.io/badge/license-${data.license}-blue.svg)
+...
+`;
+};
+
+questions()
+  .then((answers) => {
+    const readme = generateMD(answers);
+    return writeFileAsync('README.md', readme);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,48 +1,19 @@
-const fs = require("fs");
-// function that returns a license badge based on which license is passed in
-// If no license, return an empty string
-function renderLicenseBadge(license) {
+import fs from 'fs';
 
-  if (!license) {
-    return ``;
-  }
-  else {
-    return `[![${license} license](https://img.shields.io/badge/License-MIT-yellow.svg)](${renderLicenseLink(license)})`
-  }
+const renderLicenseBadge = (license) => {
+  if (!license) return '';
+  return `[![${license} license](https://img.shields.io/badge/License-${license}-yellow.svg)](https://opensource.org/licenses/${license})`;
 };
 
-// function that returns the license link
-// If no license, return an empty string
-function renderLicenseLink(license) {
-  if (license === "MIT") {
-    return `https://lbesson.mit-license.org/`
-  }
-  if (license === "GPL") {
-    return `https://www.gnu.org/licenses/gpl-3.0.en.html`
-  }
-  if (license === "CC--0") {
-    return `https://creativecommons.org/licenses/by-nd/4.0` 
-  }
+const renderLicenseSection = (license) => {
+  if (!license) return '';
+  return `## Licenses
+  This project is covered under the ${license} license. To learn more about what this means, click the license badge above.`;
 };
 
-
-
-// create a function that returns the license section of README
-// If no license, return an empty string
-function renderLicenseSection(license) {
-  if (!license) {
-    return ``;
-  } else {
-    return `## Licenses
-    This project is covered under the ${license} license. To learn more about what this means, click the license button at the top.`
-  }
-  }
-
-
-// create a function to generate markdown for README
-function generateMarkdown(data) {
+const generateMarkdown = (data) => {
   return `# ${data.title}
-  ${renderLicenseBadge(data.licenses)}
+  ${renderLicenseBadge(data.license)}
   ## Table of Contents
   * [Description](#description)
   * [Installation](#installation)
@@ -53,28 +24,25 @@ function generateMarkdown(data) {
   * [Questions](#questions)
   * [Credits](#credits)
 
-
-
   ## Description <a name="description"></a>
   ${data.description}
-## Installation <a name="installation"></a>
-${data.installation}
-## Usage <a name="usage"></a>
-${data.usage}
-## Licenses <a name="licenses"></a>
-  ${renderLicenseSection(data.licenses)}
-## Contributing <a name="contributing"></a>
-${data.contributing}
-## Tests <a name="tests"></a>
-${data.tests}
-## Questions <a name="questions"></a>
-Any questions about the project?
-Github: https://github.com/${data.github}
-Email: ${data.email}
-## Credits <a name="credits"></a>
-${data.name}
-
+  ## Installation <a name="installation"></a>
+  ${data.installation}
+  ## Usage <a name="usage"></a>
+  ${data.usage}
+  ## Licenses <a name="licenses"></a>
+  ${renderLicenseSection(data.license)}
+  ## Contributing <a name="contributing"></a>
+  ${data.contributing}
+  ## Tests <a name="tests"></a>
+  ${data.tests}
+  ## Questions <a name="questions"></a>
+  Any questions about the project?
+  Github: https://github.com/${data.github}
+  Email: ${data.email}
+  ## Credits <a name="credits"></a>
+  ${data.name}
 `;
-}
+};
 
-module.exports = generateMarkdown;
+export default generateMarkdown;
